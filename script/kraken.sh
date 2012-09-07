fde15d61790cce1f10fb42142669195553d49e5b
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-09-07 09:37:51 -0700

    polish driver script, more error checking

--- script/kraken.sh
+++ script/kraken.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-# assume $KRAKEN points to repo
-
 function usage {
   echo "
-Usage: kraken.sh [OPTIONS] <input.krn>
+Usage: kraken.sh [options] input.krn
 
 Generate Coq code and proofs from a Kraken kernel spec.
 
 OPTIONS:
   -h, --help        print this usage information
-  -f, --force       overwrite any existing output
+  -f, --force       overwrite existing output
 "
   exit 1
 }
+
+function error {
+  echo "ERROR : $1"
+  exit 1
+}
+
+if [ ! -f $KRAKEN/.kraken-root ]; then
+  error "\$KRAKEN must point to root of Kraken repository."
+fi
 
 # http://snipplr.com/view/18026/canonical-absolute-path/
 function canonpath () { 
@@ -22,9 +29,12 @@ function canonpath () {
 
 # arg defaults
 FORCE=false
-KRN=""
+INPUT=""
 
 # process args
+if [ "$*" = "" ]; then
+  usage
+fi
 while [ "$*" != "" ]; do
   case $1 in
     "-f" | "--force")
@@ -34,36 +44,33 @@ while [ "$*" != "" ]; do
       usage
       ;;
     *.krn)
-      KRN=$1
+      INPUT=$1
       ;;
     *)
-      echo "Error: unrecognized option '$1'"
+      echo "Unrecognized option '$1'"
       usage
       ;;
   esac
   shift
 done
 
-if [ ! -f "$KRN" ]; then
-  echo "Error: cannot find input '$KRN'"
-  exit 1
+if [ ! -f "$INPUT" ]; then
+  error "cannot find input '$INPUT'"
 fi
-KRN=$(canonpath "$KRN")
+INPUT=$(canonpath "$INPUT")
 
 # setup output dir
 cd $KRAKEN/scratch
-D=$(basename $KRN .krn)
+D=$(basename $INPUT .krn)
 if $FORCE; then
   rm -rf $D
 elif [ -d $D ]; then
-  echo "Error: '$D' already exists."
-  echo "To overwrite, use the --force option."
-  exit 1
+  error "'$D' already exists. To overwrite use --force."
 fi
 cp -r $KRAKEN/kernel-template $D
 
 # generate code and proofs
-$KRAKEN/bin/kraken $KRN \
+$KRAKEN/bin/kraken $INPUT \
   --turn $D/coq/Turn.v \
   --pylib $D/py/kraken_msg_lib.py
 

4358c35269da0a1be8a4680ed18532602d288247
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-09-14 18:00:52 -0700

    wip adding exchange

--- src/gen.ml
+++ src/gen.ml
@@ -148,20 +148,37 @@ let send tag_map md =
     hdr pay ret
 ;;
 
-let proto hand =
-  ""
-(*
-  let pat =
-    let p = snd hand.trigger in
-    mkstr "  | %s %s =>"
-      p.tag (String.concat " " p.payload)
+let handler_vars hand =
+  let trigger_vars t =
+    "c" :: (snd t).payload
+  in
+  let expr_vars = function
+    | Var id -> [id]
+    | _ -> []
+  in
+  let cmd_vars = function
+    | Send (_, m) ->
+        m.payload
+          |> List.map expr_vars
+          |> List.flatten
+    | Call (var, _, arg) ->
+        var :: expr_vars arg
+  in
+  let rec prog_vars = function
+    | Nop -> []
+    | Seq (c, p) -> cmd_vars c @ prog_vars p
+  in
+  uniq ((trigger_vars hand.trigger) @ (prog_vars hand.respond))
+;;
+
+let valid_exchange hand =
+  let hdr =
+    let pat = snd hand.trigger in
+    mkstr "| VE_%s :\n  forall %s,\n  ValidExchange ("
+      pat.tag (hand |> handler_vars |> String.concat " ")
   in
   let body =
-    let rec sends = function
-      | Send (_, m) -> [m]
-      | Seq (p1, p2) -> sends p1 @ sends p2
-    in
-    let rec expr_str = function
+    let expr_str_aux = function
       | Var id -> id
       | NumLit n -> mkstr "%d" n
       | StrLit s ->
@@ -170,22 +187,91 @@ let proto hand =
             |> String.concat ""
             |> mkstr "%snil"
     in
-    let aux m =
-      mkstr "    %s %s ::" m.tag
+    let expr_str e =
+      mkstr "(%s)" (expr_str_aux e)
+    in
+    let msg_str m =
+      mkstr "(%s %s)" m.tag
         (m.payload
           |> List.map expr_str
-          |> String.concat ") ("
-          |> mkstr "(%s)")
+          |> String.concat " ")
+    in
+    let cmd_trace = function
+      | Send (c, m) ->
+          mkstr "    SendMsg c %s ++" (msg_str m)
+      | Call (var, f, arg) ->
+          mkstr "    Call %s %s %s ::" (expr_str (StrLit f)) (expr_str arg) var
+    in
+    let rec loop acc = function
+      | Nop -> acc
+      | Seq (cmd, prog) ->
+          loop (cmd_trace cmd :: acc) prog
     in
     hand.respond
-      |> sends
-      |> List.map aux
+      |> loop []
       |> String.concat "\n"
-      |> mkstr "%s\n    nil"
   in
-  mkstr "%s\n%s" pat body
+  let ftr =
+    let msg_str m =
+      mkstr "(%s %s)" m.tag
+        (m.payload
+          |> String.concat " ")
+    in
+    mkstr "RecvMsg c %s)"
+      (msg_str (snd hand.trigger))
+  in
+  mkstr "%s\n%s\n%s"
+    hdr body ftr
 ;;
-*)
+
+let exchange hand =
+  let hdr =
+    let mp = snd hand.trigger in
+    mkstr "      | %s %s =>"
+      mp.tag (String.concat " " mp.payload)
+  in
+  let body =
+    let expr_str_aux = function
+      | Var id -> id
+      | NumLit n -> mkstr "%d" n
+      | StrLit s ->
+          s |> explode
+            |> List.map (mkstr "\"%c\" :: ")
+            |> String.concat ""
+            |> mkstr "%snil"
+    in
+    let expr_str e =
+      mkstr "(%s)" (expr_str_aux e)
+    in
+    let handle_cmd (acc, tr) = function
+      | Call (id, f, arg) ->
+          (mkstr "      %s <- call %s %s\n" id (expr_str (StrLit f)) (expr_str arg) ^
+           mkstr "        (tr ~~~ %s ++ tr) <@>\n" tr ^
+           mkstr "        bound c;") :: acc,
+          mkstr "Call %s %s %s :: %s" (expr_str (StrLit f)) (expr_str arg) id tr
+      | Send (_, m) ->
+          (mkstr "      sendMsg c (%s %s)\n" m.tag (m.payload |> List.map expr_str |> String.concat " ") ^
+           mkstr "        (tr ~~~ %s ++ tr);;" tr) :: acc,
+          mkstr "SendMsg c (%s %s) ++ %s" m.tag (m.payload |> List.map expr_str |> String.concat " ") tr
+    in
+    let rec loop (acc, tr) = function
+      | Nop -> 
+          ((mkstr "      {{ Return (inhabits (%s)) }}" tr) :: acc)
+            |> List.rev
+            |> String.concat "\n"
+      | Seq (c, p) ->
+          loop (handle_cmd (acc, tr) c) p
+    in
+    let tr =
+      let mp = snd hand.trigger in
+      mkstr "RecvMsg c (%s %s)"
+        mp.tag (String.concat " " mp.payload)
+    in
+    loop ([], tr) hand.respond
+  in
+  mkstr "%s\n%s"
+    hdr body
+;;
 
 (* turn template has string holes for
  *  1. declaring msg
@@ -193,7 +279,8 @@ let proto hand =
  *  3. SendMsg cases
  *  4. recvMsg cases
  *  5. sendMsg cases
- *  6. protocol cases
+ *  6. ValidExchange cases
+ *  7. exchange cases
  *)
 let turn_template = format_of_string "
 Require Import List.
@@ -218,6 +305,9 @@ Definition RecvMsg (c : chan) (m : msg) : Trace :=
       RecvNum c p0
   end.
 
+(* prevent sep tactic from unfolding *)
+Global Opaque RecvMsg.
+
 Definition SendMsg (c : chan) (m : msg) : Trace :=
   match m with
 %s
@@ -225,6 +315,9 @@ Definition SendMsg (c : chan) (m : msg) : Trace :=
     | BadTag p0 =>
       SendNum c (Num \"000\")
   end.
+
+(* prevent sep tactic from unfolding *)
+Global Opaque SendMsg.
 
 Definition recvMsg :
   forall (c : chan) (tr : [Trace]),
@@ -242,8 +335,8 @@ Proof.
     end
   );
   sep fail auto;
-    repeat rewrite app_ass; simpl;
-    sep fail auto.
+  repeat rewrite app_ass; simpl;
+  sep fail auto.
 Qed.
 
 Definition sendMsg :
@@ -262,66 +355,36 @@ Proof.
     end
   );
   sep fail auto;
-    repeat rewrite app_ass; simpl;
-    sep fail auto.
+  repeat rewrite app_ass; simpl;
+  sep fail auto.
 Qed.
 
-Fixpoint SendMsgs (c : chan) (ms : list msg) : Trace :=
-  match ms with
-    | m :: ms' => SendMsgs c ms' ++ SendMsg c m
-    | nil => nil
-  end.
-
-Definition sendMsgs :
-  forall (c : chan) (ms : list msg) (tr : [Trace]),
-  STsep (tr ~~ traced tr * bound c)
-        (fun (_ : unit) => tr ~~ traced (SendMsgs c ms ++ tr) * bound c).
-Proof.
-  intros; refine (
-    Fix2
-      (fun ms tr => tr ~~ traced tr * bound c)
-      (fun ms tr (_ : unit) => tr ~~ traced (SendMsgs c ms ++ tr) * bound c)
-      (fun self ms tr =>
-        match ms with
-          | m :: ms' =>
-            sendMsg c m
-              tr;;
-            self ms'
-              (tr ~~~ SendMsg c m ++ tr);;
-            {{ Return tt }}
-          | nil =>
-            {{ Return tt }}
-        end)
-      ms tr
-  );
-  sep fail auto;
-    repeat rewrite app_ass; simpl;
-    sep fail auto.
-Qed.
-
-Definition protocol (m : msg) : list msg :=
-  match m with
+Inductive ValidExchange : Trace -> Prop :=
 %s
-  | _ =>
-    nil
-  end.
+(* special case for errors *)
+| VE_BadTag :
+  forall c tag,
+  ValidExchange (
+    RecvMsg c (BadTag tag)).
 
-(* prevent sep tactic from unfolding *)
-Global Opaque protocol.
-
-Definition turn :
+Definition exchange :
   forall (c : chan) (tr : [Trace]),
   STsep (tr ~~ traced tr * bound c)
-        (fun (req : msg) => tr ~~ traced (SendMsgs c (protocol req) ++ RecvMsg c req ++ tr) * bound c).
+        (fun (tr' : [Trace]) => tr' ~~ tr ~~ traced (tr' ++ tr) * [ValidExchange tr'] * bound c).
 Proof.
   intros; refine (
     req <- recvMsg c
       tr;
-    sendMsgs c (protocol req)
-      (tr ~~~ RecvMsg c req ++ tr);;
-    {{ Return req }}
+    match req with
+%s
+      (* special case for errors *)
+      | BadTag tag =>
+        {{ Return (tr ~~~ RecvMsg c (BadTag tag)) }}
+    end
+
   );
-  sep fail auto.
+  sep fail auto;
+  apply himp_pure'; constructor; auto.
 Qed.
 "
 
@@ -333,7 +396,8 @@ let turn s =
     (s.msg_decl |> fmt_concat "\n" (send_trace m))
     (s.msg_decl |> fmt_concat "\n" (recv m))
     (s.msg_decl |> fmt_concat "\n" (send m))
-    (s.protocol |> fmt_concat "\n" proto)
+    (s.protocol |> fmt_concat "\n" valid_exchange)
+    (s.protocol |> fmt_concat "\n" exchange)
 
 (*
  * PYTHON MESSAGING LIBRARY

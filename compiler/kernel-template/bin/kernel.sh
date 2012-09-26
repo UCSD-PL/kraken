99233e6f6802c6d54f673cc0e164801918813449
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-09-25 22:21:38 -0700

    add debug flag to kernel.sh

--- compiler/kernel-template/bin/kernel.sh
+++ compiler/kernel-template/bin/kernel.sh
@@ -3,4 +3,8 @@
 export KROOT="__KROOT__"
 export PYTHONPATH="$PYTHONPATH:$KROOT/lib/python2.7/site-packages"
 
-$KROOT/bin/.Main
+if [ "$1" = "--debug" ]; then
+  ocamldebug -I $KROOT/ml $KROOT/bin/.Main
+else
+  $KROOT/bin/.Main
+fi

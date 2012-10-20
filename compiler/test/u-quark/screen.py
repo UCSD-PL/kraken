0095ee995b909728bfc9415244bbb8e2fb16f448
Valentin Robert <vrobert@cs.ucsd.edu>
2012-10-19 22:16:19 -0700

    is that PEP8 compliant? who knows...

--- compiler/test/u-quark/screen.py
+++ compiler/test/u-quark/screen.py
@@ -4,7 +4,8 @@ import msg, webkitlib as WK
 
 def main():
   WK.start_gtk_thread()
-  browser, web_recv, web_send = WK.synchronous_gtk_message(WK.launch_browser)('http://www.google.com')
+  browser, web_recv, web_send = (
+    WK.synchronous_gtk_message(WK.launch_browser)('http://www.google.com'))
   msg.init()
   while True:
     m = msg.recv()

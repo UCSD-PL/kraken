aa6b5dccc240bc88980c15fd0641357c5f3f61b1
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-10-19 20:52:35 -0700

    move work into main compiler tree

--- compiler/test/echo-auto-00/test.py
+++ compiler/test/echo-auto-00/test.py
@@ -5,11 +5,9 @@ import msg, time
 def main():
   msg.init()
   while True:
-    msg.send('Wget', 'http://www.google.com')
+    msg.send('M', 'Hello world!')
     m = msg.recv()
     print(m)
-    print(m[2].read())
-    m[2].close()
     time.sleep(1)
 
 main()

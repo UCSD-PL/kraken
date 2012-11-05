14fdd61aeaee867f159a6cbbc75a9c71a14c3f54
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-11-04 22:25:10 -0800

    first simple regexp examples going through

--- compiler/src/genKProp.ml
+++ compiler/src/genKProp.ml
@@ -20,6 +20,36 @@ let msgmatch_case m =
   MsgMatch MP_%s (%s %s)"
         m.tag args m.tag m.tag args
 
+let coq_of_ka_pat = function
+  | KAP_KSend s -> mkstr "KAP_KSend MP_%s" s
+  | KAP_KRecv s -> mkstr "KAP_KRecv MP_%s" s
+
+let coq_of_ka_pats kaps =
+  kaps
+    |> List.map coq_of_ka_pat
+    |> List.map (mkstr "%s :: ")
+    |> String.concat ""
+    |> mkstr "(%snil)"
+
+let rec coq_of_kt_pat = function
+  | KTP_Empty ->
+      "KTP_Empty"
+  | KTP_Class kaps ->
+      mkstr "KTP_Class %s" (coq_of_ka_pats kaps)
+  | KTP_NegClass kaps ->
+      mkstr "KTP_NegClass %s" (coq_of_ka_pats kaps)
+  | KTP_Cat (p1, p2) ->
+      mkstr "KTP_Cat (%s) (%s)"
+        (coq_of_kt_pat p1)
+        (coq_of_kt_pat p2)
+  | KTP_Alt (p1, p2) ->
+      mkstr "KTP_Alt (%s) (%s)"
+        (coq_of_kt_pat p1)
+        (coq_of_kt_pat p2)
+  | KTP_Star p ->
+      mkstr "KTP_Star (%s)"
+        (coq_of_kt_pat p)
+
 let coq_of_prop (name, prop) : string =
   match prop with
   | ImmAfter (bef, aft) ->
@@ -46,6 +76,22 @@ Proof.
   unfold ImmBefore; induction 2; simpl; intros; imm_tac.
 Qed.
 " name bef aft
+  | KTracePat p ->
+      mkstr "
+Theorem %s :
+  forall kst, KInvariant kst ->
+  forall tr, ktr kst = [tr]%%inhabited ->
+  KTraceMatch
+    (%s)
+    tr.
+Proof.
+  induction 1; [ | |
+    match goal with
+    | H: ValidExchange _ _ |- _ => inv H
+    end
+  ]; simpl; intros; uninhabit; ktm.
+Qed.
+" name (coq_of_kt_pat p)
 
 let subs s =
   List.map (fun (f, r) ->

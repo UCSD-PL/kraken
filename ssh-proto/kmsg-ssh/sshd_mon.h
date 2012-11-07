d44b2db668e9de310ca7be69e850c6d9f1cec00a
ucsd <ucsd@ucsd-VirtualBox.(none)>
2012-11-06 17:26:06 -0800

    sshd-kraken

--- ssh-proto/kmsg-ssh/sshd_mon.h
+++ ssh-proto/kmsg-ssh/sshd_mon.h
@@ -1,6 +1,6 @@
 #define SLAVE_UID 1002
 #define SSH_CONFIG_PATH "/home/ucsd/kraken/ssh-proto/kmsg-ssh/sshd_config"
-#define SSH_HOST_RSA_KEY "/usr/local/etc/ssh_host_rsa_key"
+#define SSH_HOST_RSA_KEY "/home/ucsd/kraken/ssh-proto/kmsg-ssh/ssh_host_rsa_key"
 #define SSH_SLV_PATH "/home/ucsd/kraken/ssh-proto/kmsg-ssh/sshd"
 #define SSH_SYS_PATH "/home/ucsd/kraken/ssh-proto/kmsg-ssh/sshd_sys"
 #define PTYER_SLV_PATH "/home/ucsd/kraken/ssh-proto/kmsg-ssh/ptyer"

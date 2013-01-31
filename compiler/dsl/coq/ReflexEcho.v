169ec2f65ceabab44dae8a3c4b1ec400dd89ddb7
Valentin Robert <vrobert@cs.ucsd.edu>
2013-01-31 14:31:42 -0800

    Cleaning up echo server code

--- compiler/dsl/coq/ReflexEcho.v
+++ compiler/dsl/coq/ReflexEcho.v
@@ -23,34 +23,10 @@ Definition HANDLERS : handler NB_MSG PDV :=
            (CurChan _ _ _)
            (MsgExpr _ _ m None (SLit _ _ m s, tt))
       :: nil
-    | Some impossibru => fun _ =>
-      match impossibru with end
+    | Some bad => fun _ =>
+      match bad with end
     end (pay _ _ m)
   )
 .
 
 Definition main := mk_main (Build_spec NB_MSG PDV HANDLERS).
-
-(*
-Axiom c : fd.
-
-Print kstate_run_prog. Check main.
-Eval compute in (msg NB_MSG PDV).
-Print msg.
-
-Require Import Ynot.
-
-Definition tr0 := [@nil (KAction NB_MSG PDV)]%inhabited.
-Definition ks0 := {|components := c :: nil; ktr := tr0|}.
-Definition m := (Build_msg NB_MSG PDV None (str_of_string "Hello", tt)).
-
-Eval compute in (
-  kstate_run_prog
-    NB_MSG
-    PDV
-    c
-    m
-    ks0
-    (HANDLERS m)
-).
-*)

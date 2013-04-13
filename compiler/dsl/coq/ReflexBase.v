a0071fa4420cd9f9cda916cc6519967db1da6e53
Daniel Ricketts <daniel.bmore.ricketts@gmail.com>
2013-04-12 22:06:01 -0700

    Made fin_eq_dec and num_eq Defined instead of Qed so they won't be opaque for proving policies.

--- compiler/dsl/coq/ReflexBase.v
+++ compiler/dsl/coq/ReflexBase.v
@@ -47,7 +47,7 @@ Qed.
 
 Definition num_eq (n1 n2 : num) : decide (n1 = n2).
   decide equality; apply ascii_dec.
-Qed.
+Defined.
 
 Let FALSE : num := Num "000" "000".
 Let TRUE  : num := Num "001" "000".

b7d85fd4e45972ba557b39dcdddab0d6d3305bdd
dricketts <daniel.bmore.ricketts@gmail.com>
2013-10-08 18:41:55 -0700

    Forgot to add Misc.v.

---
+++ reflex/coq/Misc.v
@@ -0,0 +1,17 @@
+Require Import List.
+Require Import Ascii.
+Require Import ReflexBase.
+Require Import Reflex.
+
+Definition dom (s:str) :=
+  let fix dom_aux s n res :=
+    match s with
+    | nil => List.rev res
+    | h::s' => if Ascii.ascii_dec h "."
+               then match n with
+                    | O => List.rev res
+                    | S n' => dom_aux s' n' (h::res)
+                    end
+               else dom_aux s' n (h::res)
+    end in
+  List.rev (dom_aux (List.rev (fst (splitAt "/" s))) 1 nil).

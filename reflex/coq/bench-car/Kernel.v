5b5190a0aec2e2c0920a50a7010d06b64ebef733
dricketts <daniel.bmore.ricketts@gmail.com>
2013-10-16 13:49:11 -0700

    New policy cruise off immediately after brakes.

--- reflex/coq/bench-car/Kernel.v
+++ reflex/coq/bench-car/Kernel.v
@@ -13,7 +13,7 @@ Open Scope string_scope.
 
 Module SystemFeatures <: SystemFeaturesInterface.
 
-Definition NB_MSG : nat := 8.
+Definition NB_MSG : nat := 10.
 
 Definition PAYD : vvdesc NB_MSG := mk_vvdesc
   [
@@ -24,10 +24,13 @@ Definition PAYD : vvdesc NB_MSG := mk_vvdesc
     ("LockDoors", []);
     ("VolumeUp",   []);
     ("VolumeDown",   []);
-    ("InflateAirbag", [])
+    ("InflateAirbag", []);
+    ("BrakesApplied", []);
+    ("CruiseOff", [])
   ].
 
-Inductive COMPT' : Type := Engine | Doors | Radio | Airbag | Alarm.
+Inductive COMPT' : Type :=
+  Engine | Brakes | CruiseCtrl | Doors | Radio | Airbag | Alarm.
 
 Definition COMPT := COMPT'.
 
@@ -39,6 +42,12 @@ Definition COMPS (t : COMPT) : compd :=
   | Engine => mk_compd
                 "Engine" "engine.c" []
                 (mk_vdesc [])
+  | Brakes => mk_compd
+                "Brakes" "brakes.c" []
+                (mk_vdesc [])
+  | CruiseCtrl => mk_compd
+                    "CruiseCtrl" "cruisectrl.c" []
+                    (mk_vdesc [])
   | Doors => mk_compd
                 "Doors"  "doors.c"      []
                 (mk_vdesc [])
@@ -61,19 +70,31 @@ Notation LockDoors       := 4%fin (only parsing).
 Notation VolumeUp       := 5%fin (only parsing).
 Notation VolumeDown      := 6%fin (only parsing).
 Notation InflateAirbag   := 7%fin (only parsing).
+Notation BrakesApplied   := 8%fin (only parsing).
+Notation CruiseOff       := 9%fin (only parsing).
 
 Definition IENVD : vcdesc COMPT := mk_vcdesc
-  [ Comp _ Engine; Comp _ Doors; Comp _ Radio; Comp _ Airbag; Comp _ Alarm ].
+  [ Comp _ Engine;
+    Comp _ Brakes;
+    Comp _ CruiseCtrl;
+    Comp _ Doors;
+    Comp _ Radio;
+    Comp _ Airbag;
+    Comp _ Alarm ].
 
 Notation v_env_engine     := 0%fin (only parsing).
-Notation v_env_doors      := 1%fin (only parsing).
-Notation v_env_radio      := 2%fin (only parsing).
-Notation v_env_airbag     := 3%fin (only parsing).
-Notation v_env_alarm      := 4%fin (only parsing).
+Notation v_env_brakes     := 1%fin (only parsing).
+Notation v_env_cruisectrl := 2%fin (only parsing).
+Notation v_env_doors      := 3%fin (only parsing).
+Notation v_env_radio      := 4%fin (only parsing).
+Notation v_env_airbag     := 5%fin (only parsing).
+Notation v_env_alarm      := 6%fin (only parsing).
 
 Definition KSTD : vcdesc COMPT := mk_vcdesc
   [ Desc _ num_d
   ; Comp _ Engine
+  ; Comp _ Brakes
+  ; Comp _ CruiseCtrl
   ; Comp _ Doors
   ; Comp _ Radio
   ; Comp _ Airbag
@@ -82,10 +103,12 @@ Definition KSTD : vcdesc COMPT := mk_vcdesc
 
 Notation v_st_crashed       := 0%fin (only parsing).
 Notation v_st_engine        := 1%fin (only parsing).
-Notation v_st_doors         := 2%fin (only parsing).
-Notation v_st_radio         := 3%fin (only parsing).
-Notation v_st_airbag        := 4%fin (only parsing).
-Notation v_st_alarm         := 5%fin (only parsing).
+Notation v_st_brakes        := 2%fin (only parsing).
+Notation v_st_cruisectrl    := 3%fin (only parsing).
+Notation v_st_doors         := 4%fin (only parsing).
+Notation v_st_radio         := 5%fin (only parsing).
+Notation v_st_airbag        := 6%fin (only parsing).
+Notation v_st_alarm         := 7%fin (only parsing).
 
 End SystemFeatures.
 
@@ -103,6 +126,10 @@ Definition INIT : init_prog PAYD COMPT COMPS KSTD IENVD :=
    seq (stupd _ IENVD v_st_crashed (i_nlit (num_of_nat 0)))
   (seq (spawn _ IENVD Engine tt v_env_engine (Logic.eq_refl _))
   (seq (stupd _ IENVD v_st_engine (i_envvar IENVD v_env_engine))
+  (seq (spawn _ IENVD Brakes tt v_env_brakes (Logic.eq_refl _))
+  (seq (stupd _ IENVD v_st_brakes (i_envvar IENVD v_env_brakes))
+  (seq (spawn _ IENVD CruiseCtrl tt v_env_cruisectrl (Logic.eq_refl _))
+  (seq (stupd _ IENVD v_st_cruisectrl (i_envvar IENVD v_env_cruisectrl))
   (seq (spawn _ IENVD Doors  tt v_env_doors  (Logic.eq_refl _))
   (seq (stupd _ IENVD v_st_doors (i_envvar IENVD v_env_doors))
   (seq (spawn _ IENVD Radio  tt v_env_radio  (Logic.eq_refl _))
@@ -110,7 +137,7 @@ Definition INIT : init_prog PAYD COMPT COMPS KSTD IENVD :=
   (seq (spawn _ IENVD Airbag  tt v_env_airbag  (Logic.eq_refl _))
   (seq (stupd _ IENVD v_st_airbag (i_envvar IENVD v_env_airbag))
   (seq (spawn _ IENVD Alarm  tt v_env_alarm  (Logic.eq_refl _))
-       (stupd _ IENVD v_st_alarm (i_envvar IENVD v_env_alarm))))))))))).
+       (stupd _ IENVD v_st_alarm (i_envvar IENVD v_env_alarm))))))))))))))).
 
 Open Scope hdlr.
 Definition HANDLERS : handlers PAYD COMPT COMPS KSTD :=
@@ -141,6 +168,10 @@ Definition HANDLERS : handlers PAYD COMPT COMPS KSTD :=
               (
                 nop
               )
+       ]]
+     | Brakes, BrakesApplied =>
+       [[ mk_vcdesc [] :
+          send (stvar v_st_cruisectrl) CruiseOff tt
        ]]
      | _, _ => [[ mk_vcdesc [] : nop ]]
     end.

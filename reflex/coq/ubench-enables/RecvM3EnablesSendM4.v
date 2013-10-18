d23ae9126e09b6b3a1415d805e74bbbe82bc83bc
Valentin Robert <vrobert@cs.ucsd.edu>
2013-10-18 16:31:01 -0700

    move fast, break things

--- reflex/coq/ubench-enables/RecvM3EnablesSendM4.v
+++ reflex/coq/ubench-enables/RecvM3EnablesSendM4.v
@@ -13,14 +13,14 @@ Require Import List.
 
 Local Opaque str_of_string.
 
-Theorem enable : forall st tr,
+Theorem RecvM3EnablesSendM4 : forall st tr,
   Reach PAYD COMPT COMPTDEC COMPS KSTD IENVD INIT HANDLERS st ->
   ktr _ _ _ _ st = inhabits tr ->
   Enables PAYD COMPT COMPS COMPTDEC
           (KORecv PAYD COMPT COMPS None
-                  (Some (Build_opt_msg PAYD M1 tt)))
+                  (Some (Build_opt_msg PAYD M3 tt)))
           (KOSend PAYD COMPT COMPS None
-                  (Some (Build_opt_msg PAYD M2 tt)))
+                  (Some (Build_opt_msg PAYD M4 tt)))
           tr.
 Proof.
   Time crush.

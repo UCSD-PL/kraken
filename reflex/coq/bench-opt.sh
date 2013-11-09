162aa21e0bd9d87e9556828ad456a437bd504362
dricketts <daniel.bmore.ricketts@gmail.com>
2013-11-08 16:36:17 -0800

    Better file names for benchmarks .

--- reflex/coq/bench-opt.sh
+++ reflex/coq/bench-opt.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# checks if branch has something pending
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+}
+
+# gets the current git branch
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+ 
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+}
+
 function run_opt {
   rm -f Opt.v
   echo "Definition prune_pol := $1." > Opt.v
@@ -7,11 +22,11 @@ function run_opt {
   echo "Definition rewrite_symb := $3." >> Opt.v
   echo "Definition ni_branch_prune := $4." >> Opt.v
   T=`date +"%y-%m-%d-%H:%M:%S"`
-  OUT=bench-$T
-  CONFIG=benchmarks/Opt-$T.v
+  GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+  OUT=bench-$T-$GIT_BRANCH
+  CONFIG=benchmarks/Opt-$OUT.v
   cp Opt.v $CONFIG
-  echo $5 >> $CONFIG
-  make bench BENCHOUT=$OUT
+
 }
 
 git diff-index --quiet HEAD
